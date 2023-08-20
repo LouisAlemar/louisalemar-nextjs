bc9df0281813435fecde59d114665f743d56c034
Louis Alemar <louisalemar@gmail.com>
2023-08-20 12:58:27 -0400

    render project names on front end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,12 @@
 "use client";
 
-import { useEffect } from "react";
-import { RootState } from "../redux/store";
-import { useSelector, useDispatch } from "react-redux";
-
 import Nav from "../components/navigation";
-import {
-  fetchProjects,
-  projectSelectors,
-} from "../redux/features/projects/projectsSlice";
-import { AppDispatch } from "../redux/store";
-import { Project } from "@/app/api/interfaces/index";
-// import handler from "@/api/route";
 
 export default function Home() {
-  // const currentNumber = useSelector(
-  //   (state: RootState) => state.projects.entities
-  // );
-  const dispatch = useDispatch<AppDispatch>();
-  const total: number = useSelector(projectSelectors.selectTotal);
-  const allProjects: Project[] = useSelector(projectSelectors.selectAll);
-  console.log({
-    total,
-    allProjects,
-  });
-
-  useEffect(() => {
-    dispatch(fetchProjects());
-  }, []);
-
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <Nav />
       <h1>Hi, Im Louis. A Software Engineer</h1>
-
-      {allProjects.forEach((data) => {
-        console.log(data);
-      })}
     </main>
   );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,30 +1,32 @@
 "use client";
 
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import Nav from "../../components/navigation";
 
-import { fetchProjects } from "../../redux/features/projects/projectsSlice";
+import {
+  fetchProjects,
+  projectSelectors,
+} from "@/redux/features/projects/projectsSlice";
 import { AppDispatch } from "../../redux/store";
-// import projects from "../pages/api/projects/projects.json";
+import { Project } from "@/app/api/interfaces/index";
 
 export default function Home() {
-  // const currentNumber = useSelector((state: RootState) => state.projects.value);
   const dispatch = useDispatch<AppDispatch>();
+  const allProjects: Project[] = useSelector(projectSelectors.selectAll);
+
   useEffect(() => {
     dispatch(fetchProjects());
-    // dispatch(fetchProjects(projects));
   }, []);
 
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <Nav />
-      <h1>Projects</h1>
-      {/* <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrease</button>
-      <button onClick={() => dispatch(reset())}>Reset</button> */}
-      {/* <h1>{currentNumber}</h1> */}
+
+      {allProjects.map((project: Project) => {
+        return <div key={project.projectId}>{project.name}</div>;
+      })}
     </main>
   );
 }
