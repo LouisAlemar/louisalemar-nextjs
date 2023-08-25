921b8dc9de08dba48b463f3111045c357f44d9cd
Louis Alemar <louisalemar@gmail.com>
2023-08-25 18:35:29 -0400

    add tailwind custom colors

--- src/components/Nav/navigation.tsx
+++ src/components/Nav/navigation.tsx
@@ -17,7 +17,7 @@ function classNames(...classes: string[]) {
 
 export default function Nav() {
   return (
-    <Disclosure as="nav" className="bg-gray-800 w-full">
+    <Disclosure as="nav" className="w-full">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
