f202412f9c9f3db601ab71caa30490cf4cfb2474
Louis Alemar <louisalemar@gmail.com>
2023-08-30 13:59:39 -0400

    add typescript error to test lint

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
 
         {allProjects.map((project: Project) => {
           return (
-            <ProjectComponent key={project.projectId} id={project.projectId} />
+            <ProjectComponent id={project.projectId} />
           );
         })}
 
