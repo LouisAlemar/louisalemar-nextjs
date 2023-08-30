69eb613518cddf768c0333a998ca4765f495c3b7
Louis Alemar <louisalemar@gmail.com>
2023-08-30 02:43:19 -0400

    change animation duration

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
-        duration: 0.8,
+        duration: 1,
         delay: 0.5,
         ease: [0, 0.71, 0.2, 1.01]
       }}
