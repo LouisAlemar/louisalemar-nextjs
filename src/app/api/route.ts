f7cd19580649f5906d75a6a943dbbd20fdae4d1e
Louis Alemar <louisalemar@gmail.com>
2023-09-04 21:17:27 -0400

    comment out my prisma code until I set up db correctly

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,23 +1,23 @@
 import * as fs from "fs";
 import * as util from "node:util";
 import * as path from "node:path";
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
-  const posts = await prisma.post.findMany({
-    where: { published: true }
-  })
+  // const posts = await prisma.post.findMany({
+  //   where: { published: true }
+  // })
 
-  // const readFile = util.promisify(fs.readFile);
+  const readFile = util.promisify(fs.readFile);
 
-  // const pathToFile = path.resolve(
-  //   __dirname,
-  //   "../../../../src/app/data/projects.json"
-  // );
+  const pathToFile = path.resolve(
+    __dirname,
+    "../../../../src/app/data/projects.json"
+  );
 
-  // const projectsData = await readFile(pathToFile, "utf8");
+  const projectsData = await readFile(pathToFile, "utf8");
 
-  return new Response(JSON.stringify(posts));
+  return new Response(projectsData);
 }
