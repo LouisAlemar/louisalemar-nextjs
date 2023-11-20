8d76bb6fdcfd80d050a568398242251996f975ca
Louis Alemar <louisalemar@gmail.com>
2023-11-20 11:15:26 -0500

    update google analytics code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,14 +23,14 @@ export default function Home() {
     <>
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}></Script>
+        src="https://www.googletagmanager.com/gtag/js?id=G-P8F5G2HMX7"></Script>
 
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.GA_ID}');
+          gtag('config', 'G-P8F5G2HMX7');
         `}
       </Script>
       <main>
